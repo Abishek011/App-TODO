1d92a1667fb9566d26bcf88cd1b6b1c0c35fb6cc
Abishek011 <rajaramsow@gmail.com>
2021-01-01 01:10:13 +0530

    ..

--- routers/routes.js
+++ routers/routes.js
@@ -162,7 +162,7 @@ var logIn = (ctx) => {
 
 //POST - adding task
 var addTask = (ctx) => {
-    console.log({mmh:ctx.verifiedData},taskData);
+    console.log({mmh:ctx.verifiedData},ctx.taskData);
     var taskDetails = {
         taskId: uuid4(),
         taskName: ctx.taskData.taskName,
