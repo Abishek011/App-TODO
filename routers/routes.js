b7c8acec390e47f6f66ce62c83a51a488e4d1b5f
Abishek011 <rajaramsow@gmail.com>
2021-01-01 16:46:47 +0530

    ..

--- routers/routes.js
+++ routers/routes.js
@@ -95,7 +95,7 @@ var signUp = async (ctx) => {
             } else {
                 //signing token for auto login
                 jwt.sign(params.Item, process.env.SIGN_TOKEN_KEY, { expiresIn: '2d' }, (err, token) => {
-                    ctx.body.cookie=token;
+                    ctx.body={cookie:token};
                     //ctx.cookies.set("signUpStatusTrue")
                 }); 
                 resolve();
